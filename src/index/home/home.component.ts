cffd46b12623006760ee3603ca4a379bc37e35ce
Zhangxu <2590000714@qq.com>
2017-06-30 20:59:37 +0800

    完善小预约手机端首页

--- src/index/home/home.component.ts
+++ src/index/home/home.component.ts
@@ -7,10 +7,23 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
-
+  pagesArr: number[] = [];
   constructor() { }
 
   ngOnInit() {
   }
 
+    ngAfterViewInit() {
+    this.getPageOffsetTop();
+  }
+
+  getPageOffsetTop(): void {
+    console.log($(".page"));
+    let self = this;
+    $(".page").each(function() {
+      self.pagesArr.push($(this).offset().top);
+      console.log(this);
+    })
+    console.log(this.pagesArr);
+  }
 }
--- src/index/layout/header/header.component.ts
+++ src/index/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'xiaoyuyue-index-header',
@@ -6,15 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class IndexHeaderComponent implements OnInit {
+  selectIndex: number;
   isShowSubMenuFlag: boolean = false;
+  href : string = location.href;
+  isDefaultSelect: boolean = this.href.substr(this.href.lastIndexOf("/") + 1, this.href.length).length > 0;
+  menuArr: string[] = ["公众号","应用场景","关于我们"];
 
-  constructor() { }
+  constructor(
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    if (!this.isDefaultSelect) {
+      this.selectIndex = 0;
+    }
   }
 
-  isShowSubMenu() {
+
+  isShowSubMenu(index?: number) {
     this.isShowSubMenuFlag = !this.isShowSubMenuFlag;
+    this.selectIndex = index;
+
+    if (index == 1) {
+      this._router.navigate(['/intro']);
+    }
   }
+
 
 }
