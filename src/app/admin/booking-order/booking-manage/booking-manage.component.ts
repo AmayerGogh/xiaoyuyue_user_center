517e733799db58c51f1ab84cb8d1a57983a4ad94
Zhangxu <2590000714@qq.com>
2017-07-27 18:56:10 +0800

    完善部分应约状态管理

--- src/app/admin/booking-order/booking-manage/booking-manage.component.ts
+++ src/app/admin/booking-order/booking-manage/booking-manage.component.ts
@@ -3,82 +3,121 @@ import { Router } from '@angular/router';
 import { PerBookingOrderServiceProxy } from "shared/service-proxies/service-proxies";
 import { AppConsts } from '@shared/AppConsts';
 import { AppComponentBase } from '@shared/common/app-component-base';
-import { Status3, BookingOrderListDto } from '@shared/service-proxies/service-proxies';
+import { Status3, BookingOrderListDto, StickedInput } from '@shared/service-proxies/service-proxies';
 import { AppStatus } from "shared/AppEnums";
 import { CancelBookingModalComponent } from "app/admin/booking-order/cancel-booking-modal/cancel-booking-modal.component";
 
 @Component({
-  selector: 'xiaoyuyue-booking-manage',
-  templateUrl: './booking-manage.component.html',
-  styleUrls: ['./booking-manage.component.scss']
+    selector: 'xiaoyuyue-booking-manage',
+    templateUrl: './booking-manage.component.html',
+    styleUrls: ['./booking-manage.component.scss']
 })
 export class BookingManageComponent extends AppComponentBase implements OnInit {
+    currentTabIndex: number = 0;
     personBookingTotalCount: number;
     personBookingDatas: BookingOrderListDto[];
-  status: Status3[] = [AppStatus.State1, AppStatus.State2, AppStatus.State5];
-  bookingName: string = "";
-  // pageSize: number = AppConsts.grid.defaultPageSize;
-  pageSize: number;
-  skip: number = 0;
-  sort: any;
-  actionFlag: boolean[] = [];
-  slogan: string = "啥都没有，赶紧去预约吧";
+    stickedInput: StickedInput = new StickedInput();
+    status: Status3[] =[AppStatus.State1, AppStatus.State2, AppStatus.State3, AppStatus.State4, AppStatus.State5];
+    bookingName: string = "";
+    // pageSize: number = AppConsts.grid.defaultPageSize;
+    pageSize: number;
+    skip: number = 0;
+    sort: any;
+    actionFlag: boolean[] = [];
+    slogan: string = "啥都没有，赶紧去预约吧";
+    bookingOrderStatusName: string[] = ["全部", "待确认", "已确认", "待评价", "已完成"];
 
-  @ViewChild('cancelBookingModal') cancelBookingModal: CancelBookingModalComponent;
+    @ViewChild('cancelBookingModal') cancelBookingModal: CancelBookingModalComponent;
 
-  constructor(
-    injector: Injector,
-    private _router: Router,
-    private _perBookingOrderServiceProxy: PerBookingOrderServiceProxy
-  ) {
-    super(
-      injector
-    );
-  }
+    constructor(
+        injector: Injector,
+        private _router: Router,
+        private _perBookingOrderServiceProxy: PerBookingOrderServiceProxy
+    ) {
+        super(
+            injector
+        );
+    }
 
-  ngOnInit() {
-    this.loadPersonBookingData();
-  }
+    ngOnInit() {
+        this.loadPersonBookingData();
+    }
     ngAfterViewInit() {
         // TODO: 暂时处理
         $("#headerTitle").text("应约管理");
     }
-  loadPersonBookingData() {
-    let state = { skip: this.skip, take: this.pageSize, sort: this.sort };
+    loadPersonBookingData() {
+        let state = { skip: this.skip, take: this.pageSize, sort: this.sort };
 
-    let maxResultCount, skipCount, sorting;
-    if (state) {
-      maxResultCount = state.take;
-      skipCount = state.skip
-      // if (state.sort.length > 0 && state.sort[0].dir) {
-      //   sorting = state.sort[0].field + " " + state.sort[0].dir;
-      // }
+        let maxResultCount, skipCount, sorting;
+        if (state) {
+            maxResultCount = state.take;
+            skipCount = state.skip
+            // if (state.sort.length > 0 && state.sort[0].dir) {
+            //   sorting = state.sort[0].field + " " + state.sort[0].dir;
+            // }
+        }
+        this._perBookingOrderServiceProxy
+            .getBookingOrders(this.bookingName, this.status, sorting, maxResultCount, skipCount)
+            .subscribe(result => {
+                this.personBookingDatas = result.items;
+                this.personBookingTotalCount = result.totalCount;
+            });
     }
 
-    this._perBookingOrderServiceProxy
-      .getBookingOrders(this.bookingName, this.status , sorting, maxResultCount, skipCount)
-      .subscribe(result => {
-        this.personBookingDatas = result.items;
-        this.personBookingTotalCount = result.totalCount;
-      });
-  }
-
-  showBookingDetail(bookingId: number) {
-    this._router.navigate(['/app/admin/order/detail', bookingId]);
-  }
-
-  cancelBooking(bookingId: number) {
-    this.cancelBookingModal.show(bookingId);
-  }
-
-  skickBooking() {
-
-  }
-
-  setActionFlag(index: number) {
-    for (let i = 0; i < this.actionFlag.length;  i++) {
-      this.actionFlag[i] = false;
+    orderSerach(index: number): void {
+        this.currentTabIndex = index;
+        if (index == 0) {
+            this.status = [AppStatus.State1, AppStatus.State2, AppStatus.State3, AppStatus.State4, AppStatus.State5];
+        } else if (index == 1) {
+            this.status = [AppStatus.State1];
+        } else if (index == 2) {
+            this.status = [AppStatus.State2];
+        } else if (index == 4) {
+            this.status = [AppStatus.State4];
+        } else if (index == 5) {
+            this.status = [AppStatus.State5];
+        } else {
+            this.message.warn("努力完善中", "敬请期待");
+        }
+        this.loadPersonBookingData();
     }
-    this.actionFlag[index] = !this.actionFlag[index];
-  }
+
+    showBookingDetail(bookingId: number) {
+        this._router.navigate(['/app/admin/order/detail', bookingId]);
+    }
+
+    cancelBooking(bookingId: number) {
+        this.cancelBookingModal.show(bookingId);
+    }
+
+    skickBooking(id): void {
+        this.stickedInput = new StickedInput();
+        this.stickedInput.id = id;
+        this.stickedInput.sticked = true;
+        this._perBookingOrderServiceProxy
+        .stickedBookingOrder(this.stickedInput)
+        .subscribe( () => {
+            this.notify.success("置顶成功");
+            this.loadPersonBookingData();
+        });
+    }
+
+    setActionFlag(index: number) {
+        for (let i = 0; i < this.actionFlag.length; i++) {
+            this.actionFlag[i] = false;
+        }
+        this.actionFlag[index] = !this.actionFlag[index];
+    }
+
+    setTipsClass(status: number): any {
+        let tipsClass = {
+            status1: status == 1,
+            status2: status == 2,
+            status3: status == 3,
+            status4: status == 4,
+            status5: status == 5
+        }
+        return tipsClass;
+    }
 }
