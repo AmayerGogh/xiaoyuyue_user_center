f0f1573bd7f37b9edb8bfe00b2400f74a2292202
Zhangxu <2590000714@qq.com>
2017-07-28 15:48:24 +0800

    更换手机逻辑

--- src/app/admin/settings/settings.routing.ts
+++ src/app/admin/settings/settings.routing.ts
@@ -5,7 +5,6 @@ import { SuggestComponent } from './suggest/suggest.component';
 import { PasswdComponent } from './security/passwd/passwd.component';
 import { EmailComponent } from './security/email/email.component';
 import { CurrentPhoneComponent } from './security/phone/current-phone/current-phone.component';
-import { NewPhoneComponent } from './security/phone/new-phone/new-phone.component';
 
 const routes: Routes = [
     {
@@ -16,8 +15,7 @@ const routes: Routes = [
             { path: 'security', component: SecurityComponent},
             { path: 'suggest', component: SuggestComponent},
             { path: 'passwd', component: PasswdComponent},
-            { path: 'current-phone', component: CurrentPhoneComponent},
-            { path: 'new-phone', component: NewPhoneComponent},
+            { path: 'change-phone', component: CurrentPhoneComponent},
             { path: 'email', component: EmailComponent}
         ]
     }
