9f3130cc4bde0adc89d09b5fa1d7b6ee6708f60a
Zhangxu <2590000714@qq.com>
2017-08-22 10:20:43 +0800

    首次获取数据请求时，替换Abp框架的Loading动画启动页

---
+++ src/shared/animations/loadingTransition.ts
@@ -0,0 +1,27 @@
+import { element } from 'protractor';
+
+// 设置进入页面的启动加载页面，替换Abp框架的Loading动画
+// tslint:disable-next-line:class-name
+export class appLoadingBusy {
+    static effectText = ['小', '预', '约', ' ', '知', '未', '来'];
+    static setBusy(): void {
+        let bodyEle = $('body');
+        let effectTextWrap = $('<div></div>').addClass('effect-text-wrap');
+        let imgLogo = $('<img>').addClass('logo').attr('src','/assets/common/images/login/logo-colorized.jpg');
+        let appLoadingBusyWrap = $('<div></div>').addClass('app-loading-busy-wrap');
+
+        bodyEle.append(appLoadingBusyWrap);
+        appLoadingBusyWrap.append(imgLogo);
+        appLoadingBusyWrap.append(effectTextWrap);
+
+        appLoadingBusy.effectText.forEach(element => {
+            let spanEle = $('<span></span>').text(element).addClass('effect-text');
+            effectTextWrap.append(spanEle);
+        });
+        console.log(appLoadingBusyWrap);
+    };
+
+    static clearBusy(): void {
+       $('.app-loading-busy-wrap').remove();
+     };
+};
