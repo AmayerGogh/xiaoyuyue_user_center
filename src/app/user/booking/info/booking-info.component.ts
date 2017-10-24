8ba033cc19e4390972f2f2c04fbbabcb534a3fc1
Zhangxu <2590000714@qq.com>
2017-10-24 15:48:28 +0800

    优化PictureUrlHelper

--- src/app/user/booking/info/booking-info.component.ts
+++ src/app/user/booking/info/booking-info.component.ts
@@ -8,43 +8,47 @@ import { PictureUrlHelper } from 'shared/helpers/PictureUrlHelper';
 import { appModuleAnimation } from 'shared/animations/routerTransition';
 
 @Component({
-  selector: 'xiaoyuyue-booking-info',
-  templateUrl: './booking-info.component.html',
-  styleUrls: ['./booking-info.component.scss'],
-  animations: [appModuleAnimation()]
+    selector: 'xiaoyuyue-booking-info',
+    templateUrl: './booking-info.component.html',
+    styleUrls: ['./booking-info.component.scss'],
+    animations: [appModuleAnimation()]
 })
 export class BookingInfoComponent extends AppComponentBase implements OnInit, AfterViewInit {
-  bookingOrderForEdidData: GetPersonBookingOrderOutput;
+    bookingOrderForEdidData: GetPersonBookingOrderOutput;
 
-  href: string = document.location.href;
-  bookingId;
+    href: string = document.location.href;
+    bookingId;
+    outletPictureUrl: string = '/assets/common/images/admin/booking-bg.jpg';
 
-  constructor(
-    injector: Injector,
-    private _route: ActivatedRoute,
-    private _perBookingOrderServiceProxy: PerBookingOrderServiceProxy
-  ) {
-    super(injector);
-    this.bookingId = this._route.snapshot.paramMap.get('id');
-  }
+    constructor(
+        injector: Injector,
+        private _route: ActivatedRoute,
+        private _perBookingOrderServiceProxy: PerBookingOrderServiceProxy
+    ) {
+        super(injector);
+        this.bookingId = this._route.snapshot.paramMap.get('id');
+    }
 
-  ngOnInit() {
-    this.loadBookingOrderForEditData(this.bookingId);
-  }
+    ngOnInit() {
+        this.loadBookingOrderForEditData(this.bookingId);
+    }
 
-  ngAfterViewInit() {
-    // TODO: 暂时处理
-    $('#headerTitle').text('应约详情');
-  }
+    ngAfterViewInit() {
+        // TODO: 暂时处理
+        $('#headerTitle').text('应约详情');
+    }
 
-  loadBookingOrderForEditData(bookingId: number) {
-    this._perBookingOrderServiceProxy
-      .getBookingOrderForEdit(bookingId)
-      .subscribe(result => {
-        this.bookingOrderForEdidData = result;
-        this.bookingOrderForEdidData.bookingInfo.outletPictureUrl =
-          PictureUrlHelper.getOutletPicCompressUrl(this.bookingOrderForEdidData.bookingInfo.outletPictureUrl);
-      })
-  }
+    loadBookingOrderForEditData(bookingId: number) {
+        this._perBookingOrderServiceProxy
+            .getBookingOrderForEdit(bookingId)
+            .subscribe(result => {
+                this.bookingOrderForEdidData = result;
+                this.bookingOrderForEdidData.bookingInfo.outletPictureUrl = this.getOutletPictureUrl();
+            })
+    }
+
+    private getOutletPictureUrl(): string {
+        return this.bookingOrderForEdidData.bookingInfo.outletPictureUrl === '' ? '' : PictureUrlHelper.getOutletPicCompressUrl(this.bookingOrderForEdidData.bookingInfo.outletPictureUrl);
+    }
 
 }
--- src/shared/helpers/PictureUrlHelper.ts
+++ src/shared/helpers/PictureUrlHelper.ts
@@ -2,18 +2,18 @@ import { MediaCompressFormat } from 'shared/AppConsts';
 
 export class PictureUrlHelper {
     static getCompressUrl(url: string, compressProt: string): string {
-        return url + '?' + compressProt;
+        return url === '' ? url : url + '?' + compressProt;
     }
 
     static getBookingPicCompressUrl(url: string): string {
-        return url + '?' + MediaCompressFormat.bookingPictureFormat;
+        return url === '' ? url : url + '?' + MediaCompressFormat.bookingPictureFormat;
     }
 
     static getTimelinePicCompressUrl(url: string): string {
-        return url + '?' + MediaCompressFormat.timelinePictureFormat;
+        return url === '' ? url : url + '?' + MediaCompressFormat.timelinePictureFormat;
     }
 
     static getOutletPicCompressUrl(url: string): string {
-        return url + '?' + MediaCompressFormat.outletPictureFormat;
+        return url === '' ? url : url + '?' + MediaCompressFormat.outletPictureFormat;
     }
 }
