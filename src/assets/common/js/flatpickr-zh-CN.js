672b2b36ab300373fb396c4fa15265e7492746ed
Victor_Lo <694978795@qq.com>
2018-01-05 18:07:53 +0800

    日历选择控件添加多语言处理

--- src/assets/common/js/flatpickr-zh-CN.js
+++ src/assets/common/js/flatpickr-zh-CN.js
@@ -1,14 +1,16 @@
 /* Mandarin locals for flatpickr */
-var flatpickr = flatpickr || { l10ns: {} };
+var flatpickr = flatpickr || {
+    l10ns: {}
+};
 flatpickr.l10ns.zh = {};
 
 flatpickr.l10ns.zh.weekdays = {
-	shorthand: ["日", "一", "二", "三", "四", "五", "六"],
+    shorthand: ["日", "一", "二", "三", "四", "五", "六"],
     longhand: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
 };
 
 flatpickr.l10ns.zh.months = {
-	shorthand: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+    shorthand: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
     longhand: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
 };
 
