dd19291d9f4f32f1778b50cd2b70342d18415c15
Zhangxu <2590000714@qq.com>
2018-01-05 19:22:09 +0800

    移除fullpage插件，完善首页滚屏

--- src/app/index/desktop/layout/header/header.component.ts
+++ src/app/index/desktop/layout/header/header.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit, Injector } from '@angular/core';
 
 import { AppConsts } from '@shared/AppConsts';
 import { AppComponentBase } from 'shared/common/app-component-base';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
     selector: 'xiaoyuyue-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class DesktopHeaderComponent extends AppComponentBase implements OnInit {
+export class DesktopHeaderComponent extends AppComponentBase implements OnInit, AfterViewInit {
+    scrollSectionArr: number[] = [];
 
     constructor(
         private injector: Injector
@@ -19,11 +21,26 @@ export class DesktopHeaderComponent extends AppComponentBase implements OnInit {
     ngOnInit() {
     }
 
+    ngAfterViewInit() {
+        this.initPageSections();
+    }
+
     login(): void {
         window.location.href = AppConsts.businessCenterUrl + '/auth/login';
     }
 
     register(): void {
         window.location.href = AppConsts.businessCenterUrl + '/auth/register';
+    }
+
+    public scrolled(index: number): void {
+        let scrollTop = this.scrollSectionArr[index] - 80;
+        $('html, body').animate({ scrollTop: scrollTop }, 300);
+    }
+
+    private initPageSections(): void {
+        $.each($('.section'), (i, data) => {
+            this.scrollSectionArr.push($('.section').eq(i).position().top);
+        });
     }
 }
